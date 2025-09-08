b5a1cd6cf40fd919b10ca13950164dac7d4d9235
bojo500 <flavasava1212@gmail.com>
2025-09-08 00:53:10 -0500

    fix some docker configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DATABASE_HOST || 'db',
+    port: parseInt(process.env.DATABASE_PORT || '3306', 10),
+    username: process.env.DATABASE_USER || 'admin',
+    password: process.env.DATABASE_PASSWORD || 'password',
+    database: process.env.DATABASE_NAME || 'dataone',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
